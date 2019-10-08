b197c14e30bd4b880cb04450d5c3ee73757cc46f
GiorgioMariani <giorgiomariani94@gmail.com>
2019-10-08 17:57:25 +0200

    update

--- src/task2.py
+++ src/task2.py
@@ -12,7 +12,6 @@ import tqdm
 import visualization
 import misc
 
-
 def keyword_similarity(keyword1:str, keyword2:str, method="jaccard") -> float:
     """
     This function computes a simple word similarity between keywords.
@@ -28,11 +27,12 @@ def keyword_similarity(keyword1:str, keyword2:str, method="jaccard") -> float:
     elif method == "equal":
         return float(keyword1==keyword2)
 
-def create_topic_mapping(
+def create_topic_links(
     g1:nx.DiGraph, g2:nx.DiGraph, 
     T1:np.ndarray, T2:np.ndarray, 
     mapping_method="chamfer",
-    word_sim_method="jaccard") -> list:
+    word_sim_method="jaccard",
+    mode="full") -> list:
 
     n1 = g1.number_of_nodes()
     n2 = g2.number_of_nodes()
@@ -76,6 +76,7 @@ def create_topic_mapping(
             maski = T1[:,ti]>0
             similarityi = S[T1[:,ti]>0, :]
             sumi = maski.sum()
+            
             for tj in range(tn2):
                 maskj = T2[:,tj]>0
                 similarityij = similarityi[:,maskj]
@@ -84,64 +85,99 @@ def create_topic_mapping(
     else:
         raise Exception("Wrong mapping method: available values for topic mapping method: 'jaccard', 'vector', and 'chamfer'!")
 
-    # remove score that is too low
+    # low scores removal
     score[score<0.2] = 0
 
     # compute mapping
-    g1_map_g2 = score.argmax(axis=1) # array containing mapping between topics in g1 to topics in g2
-    g2_map_g1 = score.argmax(axis=0) # array containing mapping between topics in g2 to topics in g1
-
-    # REMOVEME visualization of mapping ------------------------------
-    #visualization.show_word_similarity(g1=g1, g2=g2, wordsimilarity=score)
-    tn1, tn2 = score.shape
-    X = np.zeros([tn1,tn2])
-    X[range(tn1), g1_map_g2] = score[range(tn1), g1_map_g2]
-    X[g2_map_g1, range(tn2)] += score[g2_map_g1, range(tn2)]
-    #visualization.show_word_similarity(g1=g1, g2=g2, wordsimilarity=X)
-
-    scorelist = []
-    for ti in range(tn1):
-        for tj in range(tn2):
-            wordsi = set(misc.get_topic_keywords(g1,T1[:,ti]))
-            wordsj = set(misc.get_topic_keywords(g2,T2[:,tj]))
-            if score[ti,tj] != 0:
-                scorelist.append( (score[ti,tj], len(wordsi), len(wordsj), ti, tj))
-    scorelist.sort()
-    #-----------------------------------------------------------------
-
     mapping = []
-    for t1, t2 in enumerate(g1_map_g2):
-        if score[t1,t2] != 0:
-            mapping.append( (t1, t2, score[t1,t2]) )
+    if mode == "reduced": # reduced mode --------------------------------------
+        g1_map_g2 = score.argmax(axis=1) # array containing mapping between topics in g1 to topics in g2
+        g2_map_g1 = score.argmax(axis=0) # array containing mapping between topics in g2 to topics in g1
 
-    for t2, t1 in enumerate(g2_map_g1):
-        if score[t1,t2] != 0:
-            mapping.append( (t1, t2, score[t1,t2]) )
+        '''
+        # REMOVEME visualization of mapping 
+        #visualization.show_word_similarity(g1=g1, g2=g2, wordsimilarity=score)
+        tn1, tn2 = score.shape
+        X = np.zeros([tn1,tn2])
+        X[range(tn1), g1_map_g2] = score[range(tn1), g1_map_g2]
+        X[g2_map_g1, range(tn2)] += score[g2_map_g1, range(tn2)]
+        #visualization.show_word_similarity(g1=g1, g2=g2, wordsimilarity=X)
+        '''
+        # compute mapping
+        for t1, t2 in enumerate(g1_map_g2):
+            if score[t1,t2] != 0:
+                mapping.append( (t1, t2, score[t1,t2]) )
+        for t2, t1 in enumerate(g2_map_g1):
+            if score[t1,t2] != 0:
+                mapping.append( (t1, t2, score[t1,t2]) )
+
+    elif mode == "full": # full mode ------------------------------------------
+        for ti in range(tn1):
+            for tj in range(tn2):
+                if score[ti][tj] > 0:
+                    mapping.append( (ti, tj, score[ti,tj]) )
+    else:
+        raise Exception("Wrong link generation mode: available values are: 'full' or 'reduced'!")
     return mapping
 
-def create_chain(
-    path:list, 
-    keyword_graphs: Dict[int,nx.DiGraph],
-    keyword_topics: Dict[int,np.ndarray],
-    directory:str):
+# =============================================================================
+def get_path(tdag:nx.DiGraph, s):
+    alpha = 1/3
+    path = [s]
+    sumscore = 0
+    while True:
+        maxscore = 0
+        argmax = None
+        u = path[-1]
 
-    #create output directory
-    if os.path.exists(directory):
-        import shutil
-        shutil.rmtree(directory)
-    os.makedirs(directory)
-    
-    for u in path:
-        y, ti = u
-        g = keyword_graphs[y]
-        t = keyword_topics[y][:,ti]
-        f = os.path.join(directory, str(y)+".png")
-        visualization.create_topic_picture_gradient(g=g, topic=t, file=f)
+        # control successors
+        for v in tdag.successors(u):
+            score = 0
+            for z in reversed(path):
+                x =  tdag[z][v]["map_strength"] if tdag.has_edge(z,v) else 0
+                score = alpha*x + (1-alpha)*score
+            if score >= maxscore:
+                argmax = v
+                maxscore = score 
+
+        # check loop conditions
+        if argmax is not None:
+            path.append(argmax)
+            sumscore += maxscore
+        else:
+            score = sumscore#/np.sqrt(len(path))
+            words = set().union(*[tdag.nodes[u]["topic"] for u in path])
+            return score, path, words
+
+def chains_extraction(tdag, k=20):
+    '''
+    tdag = tdag.copy()
+    paths = []
+    for i in tqdm.trange(k):
+        longest_path:nx.DiGraph = nx.dag_longest_path(tdag, weight='map_strength', default_weight=None)
+        paths.append(longest_path)
+        tdag.remove_nodes_from(longest_path)
+    return paths
+    '''
+    paths = []
+    for y,t in tqdm.tqdm(tdag):
+        score, path, words = get_path(tdag, s=(y,t))
+        paths.append( (score, path, words) )
+
+    top_paths = []
+    for _ in tqdm.tqdm(range(k)):
+        score, top_path, top_words = max(paths)
+        top_paths.append(top_path)
+        for i in range(len(paths)):
+            score, path, words = paths[i]
+            s = len(top_words.intersection(words))/len(top_words.union(words))
+            paths[i] = score*(1-s), path, words
+
+    return top_paths
+
 
 #========================================================================================
-def load_topics(topic_directory:str) -> Tuple[
-                                                                                Dict[int, nx.DiGraph], 
-                                                                                Dict[int, np.ndarray]]:
+def load_topics(topic_directory:str) -> Tuple[Dict[int, nx.DiGraph], Dict[int, np.ndarray]]:
     """
     This function loads into memory the graphs and topics generated during task1.
     """
@@ -153,11 +189,10 @@ def load_topics(topic_directory:str) -> Tuple[
     for name in tqdm.tqdm(os.listdir(topic_directory_absolute)):
         year_directory_abs = os.path.join(topic_directory_absolute, name)
         if os.path.isdir(year_directory_abs):
-            if name != "chains":
+            if name.isdigit():
                 year = int(name)
                 graph_filename = os.path.join(year_directory_abs, "graph.pickle")
                 topic_filename = os.path.join(year_directory_abs, "topics.npy")
-
                 graphs_per_year[year] = nx.read_gpickle(graph_filename)
                 topics_per_year[year] = np.load(topic_filename)
     print("topics loaded.")
@@ -165,11 +200,16 @@ def load_topics(topic_directory:str) -> Tuple[
 
 def task2(
     parent_folder:str="topics", 
-    topic_folder:str="last", 
-    visualize=False,
-    mapping_method="chamfer",
-    word_sim_method="jaccard"):
+    topic_folder:str="last",
+    **kwargs):
 
+    topic_score = "chamfer" if "topic_score" not in kwargs else kwargs["topic_score"]
+    word_similarity = "jaccard" if "word_similarity" not in kwargs else kwargs["word_similarity"]
+    visualize = False if "visualize" not in kwargs else kwargs["visualize"]
+    chain_number = 0 if "chain_number" not in kwargs else kwargs["chain_number"]
+    load_dag = False if "load_dag" not in kwargs else kwargs["load_dag"]
+
+    # solve output directory
     if topic_folder is None:
         topic_directory = os.path.abspath(parent_folder)
     else:
@@ -178,9 +218,9 @@ def task2(
             topic_folder=max(os.listdir(parent_directory_absolute))
         topic_directory = os.path.join(parent_directory_absolute, topic_folder)
 
+    # load stored topics
     gperyear, tperyear = load_topics(topic_directory)
     years:List[int] = [year for year in tperyear.keys()]
-    topic_DAG = nx.DiGraph()
 
     def _create_DAG_nodes(g:nx.Graph, topics:np.ndarray, year:int) -> list:
         nodes_DAG = []
@@ -202,32 +242,44 @@ def task2(
             edges.append( (source_node, target_node, attr) )
         return edges
 
-    # add nodes in the first year
-    firstyear = years[0]
-    firstgraph = gperyear[firstyear]
-    firsttopic = tperyear[firstyear]
-    topic_DAG.add_nodes_from(_create_DAG_nodes(firstgraph, firsttopic, firstyear))
+    dag_file = os.path.join(topic_directory,"topic-DAG.pickle")
+    if load_dag:
+        topic_DAG = nx.read_gpickle(dag_file)
+    else:
+        # add nodes to graph ------------------------------------------------------
+        print("creating topic DAG:\nadding nodes to the topic DAG ...")
+        topic_DAG = nx.DiGraph()
+        for i in range(len(years)):
+            y = years[i]
+            g, t = gperyear[y], tperyear[y]
+            topic_DAG.add_nodes_from(_create_DAG_nodes(g, t, y))
 
-    # add leyers to the DAG
-    for i in tqdm.trange(len(years)-1):
-        # get years
-        year1 = years[i]
-        year2 = years[i+1]
-        # load graphs
-        g1 = gperyear[year1]
-        g2 = gperyear[year2]
-        # load topics
-        T1 = tperyear[year1]
-        T2 = tperyear[year2]
-        mapping = create_topic_mapping(
-            g1,g2,T1,T2,
-            mapping_method=mapping_method,
-            word_sim_method=word_sim_method)
-        # add nodes and edges to the DAG
-        topic_DAG.add_nodes_from(_create_DAG_nodes(g2, T2, year2))
-        topic_DAG.add_edges_from(_create_DAG_edges(mapping, year1, year2))
-    
-    # REMOVE show the topic DAG----------------------------------------
+        # add leyers to the DAG ---------------------------------------------------
+        print("adding edges to the topic DAG ...")
+        bar = tqdm.tqdm(total=len(years)*(len(years)-1)/2)
+        for i in range(len(years)-1):
+            for j in range(i+1, len(years)):
+                bar.update()
+                # get years
+                y1, y2 = years[i], years[j]
+                # load graphs
+                g1, g2 = gperyear[y1], gperyear[y2]
+                # load topics
+                T1, T2 = tperyear[y1], tperyear[y2]
+                mapping = create_topic_links(
+                    g1,g2,T1,T2,
+                    mapping_method=topic_score,
+                    word_sim_method=word_similarity)
+                # add nodes and edges to the DAG
+                topic_DAG.add_edges_from(_create_DAG_edges(mapping, y1, y2))
+
+        # write DAG into file
+        nx.write_gpickle(topic_DAG, dag_file)
+
+    # compute chains ----------------------------------------------------------
+    paths = chains_extraction(topic_DAG)
+
+    # show the topic DAG ------------------------------------------------------
     if visualize:
         import matplotlib.cm as cm
         node_color = [y for y,ti in topic_DAG.nodes()]
@@ -245,33 +297,49 @@ def task2(
         plot.show()
     #----------------------------------------------------------------------
 
-    # compute chains ------------------------------------------------------
-    tmp_dag = topic_DAG.copy()
+    # create directory for chains
     chains_folder = "chains"
     chains_directory = os.path.join(topic_directory, chains_folder)
     if os.path.exists(chains_directory):
         import shutil
         shutil.rmtree(chains_directory)
     os.makedirs(chains_directory)
-    for i in tqdm.trange(10):
-        longest_path:nx.DiGraph = nx.dag_longest_path(tmp_dag, weight='map_strength', default_weight=None)
-        tmp_dag.remove_nodes_from(longest_path)
-
-        chain_directory = os.path.join(chains_directory, "chain_"+str(i))
+    # store chains
+    print("storing chains ...")
+    for i,p in enumerate(tqdm.tqdm(paths)):
         # store heaviest chain
-        create_chain(
-            path=longest_path,
-            keyword_graphs=gperyear,
-            keyword_topics=tperyear,
-            directory=chain_directory)
+        chain_directory = os.path.join(chains_directory, "chain_"+str(i))
+        os.makedirs(chain_directory)
+        for u in p:
+            y, ti = u
+            g, t = gperyear[y], tperyear[y][:,ti]
+            f = os.path.join(chain_directory, str(y)+".png")
+            visualization.create_topic_picture_gradient(g=g, topic=t, file=f)
 
-def create_macrotopic(path, keyword_graphs, keyword_topics):
-    return
-
+    # create time-fused topics
+    merged_topics = []
+    from collections import Counter
+    for p in paths:
+        merged_keywords = Counter()
+        for u in p:
+            words = topic_DAG.nodes[u]["topic"]
+            for w in words:
+                merged_keywords[w] += 1
+        merged_topics.append(merged_keywords)
+    
+    # store final macro topics
+    print("storing fused-topics ...")
+    for topic in tqdm.tqdm(merged_topics):
+        from wordcloud import WordCloud
+        wcloud = WordCloud(background_color="white")
+        wcloud.generate_from_frequencies(topic)
+        
+        # show
+        plot.imshow(wcloud, interpolation="bilinear")
+        plot.axis("off")
+        plot.show()
 
 
 #------------------------------------------------------------------------------
 if __name__ == "__main__":
-    task2(visualize=False, word_sim_method="equal")
-
-
+    task2(visualize=False, word_similarity="equal", load_dag=False)
